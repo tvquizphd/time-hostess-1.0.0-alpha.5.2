ad579718f1656228e60c64bf809a325d19b0f337
TVQuizPhD <tvquizphd@gmail.com>
2021-12-03 22:47:06 +0000

    mobile issues

---
+++ _next/static/EUAENyket_kCFjJqA7InA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/[[...slug]]":["static/chunks/80-c6433cdd20612f18.js","static/css/6ea248c492395ce1.css","static/chunks/pages/[[...slug]]-6a0701f0dfdba533.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
